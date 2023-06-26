dcac2e53e1356525768357d5636fdcf585f11dfd
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-06-26 15:43:26 -0600

    added user login

--- Astrapply/src/components/navBar.tsx
+++ Astrapply/src/components/navBar.tsx
@@ -2,11 +2,35 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "../css/navbar.css";
 
 function NavBar() {
-  const { loginWithRedirect, logout } = useAuth0();
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  console.log(isAuthenticated);
+  if (isAuthenticated && user) {
+    return (
+      <div className="navWrapper">
+        <nav className="nav">
+          <div className="navbar-left">Astrapply</div>
+
+          <div className="middleDiv">Welcome, {user.username}</div>
+
+          <div className="navbar-right">
+            <button onClick={() => loginWithRedirect()}>Log in</button>
+            <button onClick={() => logout()}>Log Out</button>
+          </div>
+        </nav>
+      </div>
+    );
+  }
   return (
     <div className="navWrapper">
       <nav className="nav">
         <div className="navbar-left">Astrapply</div>
+
         <div className="navbar-right">
           <button onClick={() => loginWithRedirect()}>Log in</button>
           <button onClick={() => logout()}>Log Out</button>
